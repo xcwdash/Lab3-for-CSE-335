4eaa8ac0a4cfa0c2f81897583fc143599e324bcd
Zachary <zach.lee96@gmai.com>
2024-02-11 18:58:11 -0700

    Initial Commit

---
+++ Lab3/Lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab3App.swift
+//  Lab3
+//
+//  Created by Zachary Lee on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
